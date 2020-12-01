66d733aec0f8a6d184e9a115823a7a3faa318cb3
m2m <hkfreund@live.de>
2020-12-01 06:53:01 -0800

    Update importldd.py

--- importldd.py
+++ importldd.py
@@ -865,7 +865,8 @@ class Converter:
                 if not (len(pa.Bones) > flexflag):
                 # Flex parts don't need to be moved
                     #out.write('\t\t\tmatrix4d xformOp:transform = ( ({0}, {1}, {2}, {3}), ({4}, {5}, {6}, {7}), ({8}, {9}, {10}, {11}), ({12}, {13}, {14}, {15}) )\n'.format(n11, n12, n13, n14, n21, n22, n23, n24, n31, n32, n33, n34, n41, n42 ,n43, n44))
-                    transform_matrix = mathutils.Matrix(((n11, n12, n13, n14),(n21, n22, n23, n24),(n31, n32, n33, n34),(n41, n42, n43, n44)))
+                    #transform_matrix = mathutils.Matrix(((n11, n12, n13, n14),(n21, n22, n23, n24),(n31, n32, n33, n34),(n41, n42, n43, n44)))
+                    transform_matrix = mathutils.Matrix(((n11, n21, n31, n41),(n12, n22, n32, n42),(n13, n23, n33, n43),(n14, n24, n34, n44)))
 
                     
                     # Random Scale for brick seams
@@ -1010,6 +1011,10 @@ class Converter:
                     #gop.close()
                     edges = []
                     mesh.from_pydata(verts, edges, faces)
+                    
+                    obj.matrix_world = transform_matrix
+                    #obj.matrix_world=((1,0,0,0),(0,.707,.707,0),(0,-.707,.707,0),(1,0,0,1))
+
                    
 
                 #Logo on studs
