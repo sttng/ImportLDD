14118641ccdfa7b4f8d0934fdc3b9ebb42df70ac
m2m <hkfreund@live.de>
2020-12-08 05:37:10 -0800

    Update importldd.py

--- importldd.py
+++ importldd.py
@@ -1043,7 +1043,6 @@ class Converter:
             
                 if not (len(pa.Bones) > flexflag):
                 # Flex parts don't need to be moved, but non-flex parts need
-                    #transform_matrix = mathutils.Matrix(((n11, n12, n13, n14),(n21, n22, n23, n24),(n31, n32, n33, n34),(n41, n42, n43, n44)))
                     transform_matrix = mathutils.Matrix(((n11, n21, n31, n41),(n12, n22, n32, n42),(n13, n23, n33, n43),(n14, n24, n34, n44)))
 
                     # Random Scale for brick seams
@@ -1093,10 +1092,8 @@ class Converter:
                         if usenormal == True: # write normals in case flag True
                             # WARNING: SOME PARTS MAY HAVE BAD NORMALS. FOR EXAMPLE MAYBE PART: (85861) PL.ROUND 1X1 W. THROUGHG. HOLE
                             #gop.write('\t\tnormal3f[] normals = [')
-                            fmt = ""
                             for normal in geo.Parts[part].outnormals:
                                 #gop.write('{0}({1}, {2}, {3})'.format(fmt, normal.x, normal.y, normal.z))
-                                fmt = ", "
 
                         faces = []
                         for face in geo.Parts[part].faces:
